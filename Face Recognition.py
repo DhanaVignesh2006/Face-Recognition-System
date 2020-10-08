9ca76150e598641bf4978e06475cd0fff7df3719
Dhana _Vignesh_Sice_2006 <68023678+DhanaVignesh2006@users.noreply.github.com>
2020-10-08 11:21:53 +0530

    Create Face Recognition.py

---
+++ Face Recognition.py
@@ -0,0 +1,72 @@
+# This is program will works like encode the faces in the specified directory and check if it is match with the frame streaming from webcam
+"""These are the modules we want to make Face-Recognition-System
+  cmake - pip install cmake 
+  opencv - pip install opencv-python
+  numpy - pip install numpy
+  face_recognition - pip install face_recognition
+  dlib - pip install https://pypi.python.org/packages/da/06/bd3e241c4eb0a662914b3b4875fc52dd176a9db0d4a2c915ac2ad8800e9e/dlib-19.7.0-cp36-cp36m-win_amd64.whl#md5=b7330a5b2d46420343fbed5df69e6a3f
+"""
+
+# importing modules
+import cv2  
+import numpy as np 
+import face_recognition 
+
+# name of the path where we stored the images
+path = "faces"
+images = []
+classNames = []
+myList = os.listdir(path)
+for cl in myList:
+    curImg = cv2.imread(f'{path}\{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+
+def findEncodings(image):
+    encodelist = []
+    for imag in image:
+        imag = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(imag)[0]
+        encodelist.append(encode)
+    return encodelist
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(1)
+
+cap.set(cv2.CAP_PROP_FPS, 10)
+
+while True:
+    success, img = cap.read()
+    imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgs)
+    encodesCurFrame = face_recognition.face_encodings(imgs, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].lower()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
+            
+                    else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
+            cv2.putText(img, "Unknown", (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows
