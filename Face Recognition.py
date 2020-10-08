ab85a5dec0443ac588b32d3ed7fd34e2c8ab4f99
Dhana _Vignesh_Sice_2006 <68023678+DhanaVignesh2006@users.noreply.github.com>
2020-10-08 11:36:36 +0530

    Update Face Recognition.py

--- Face Recognition.py
+++ Face Recognition.py
@@ -60,7 +60,7 @@ while True:
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
             
-         else:
+        else:
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
